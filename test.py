9bc1052aa09e75caa0324e027884f5a51956176f
GlebAQA <galievgleb99@gmail.com>
2023-08-11 10:52:24 +0300

    class OnlineStore

--- test.py
+++ test.py
@@ -1,26 +1,3 @@
-class Cat:
-    def __init__(self, name, age, color, is_sleeping=False):
-        self.name = name
-        self.age = age
-        self.color = color
-        self.is_sleeping = is_sleeping
+class OnlineStore:
 
-    def sleep(self):
-        self.is_sleeping = True
-
-    def wake_up(self):
-        self.is_sleeping = False
-
-    def make_sound(self):
-        if self.is_sleeping:
-            print("Zzz...")
-        else:
-            print("Мяу!")
-
-
-kiska1 = Cat("kiska1", 10, "green")
-kiska1.make_sound()
-kiska1.sleep()
-kiska1.make_sound()  # Выведет: Zzz...
-kiska1.wake_up()
-kiska1.make_sound()  # Выведет: Мяу!
+    def __init__(self, name, )
